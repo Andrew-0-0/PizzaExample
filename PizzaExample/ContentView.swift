7b9ae9359829db90b0f5ffbbcb53aa4cc05e83ce
Andrew-0-0 <andrey.ibragimov@gmail.com>
2026-08-18 09:58:39 +0200

    Initial commit

---
+++ PizzaExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PizzaExample
+//
+//  Created by Andrew I on 18.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PizzaExample/PizzaExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaExampleApp.swift
+//  PizzaExample
+//
+//  Created by Andrew I on 18.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct PizzaExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
